43150b7ac6be67d26d33d93b09fb5a77eb8211b4
Aimeelr <aimeelynnramirez@gmail.com>
2017-10-04 19:10:31 -0400

    api to call

--- assets/scripts/list/api.js
+++ assets/scripts/list/api.js
@@ -11,13 +11,13 @@ const getPosts = function (data) {
     dataType: 'json',
     success: function (response, textStatus, jqXhr) {
       const posts = response.posts
-      console.log(post.length)
+      console.log(posts.length)
       let count = 0
       for (let i = 0; i < posts.length; i++) {
-        const title = post[i].title
-        const body = post[i].body
+        const title = posts[i].title
+        const body = posts[i].body
         count++
-        if (count % 4 === 0) {
+        if (count % 2 === 0) {
           $('#posts').append('<div class="col-md-1"></div><div class= "posts">' + '<h3>' + title + '</h3><h3>'+ body + '</h3>' + '<br><br> <button type="button" class="btn  btn-lg center">Posts</button> '+ '</div>')
         } else {
           $('#posts').append('<div class="col-md-1"></div><div class= "posts">' + '<h3>' + title + '</h3><h3>' + body + '</h3>'+ '<br><br> <button type="button" class="btn  btn-lg center">Posts</button> '+ '</div>')
